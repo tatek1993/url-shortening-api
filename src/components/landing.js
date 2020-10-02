cbee4c2eea6502018c87ab8349d077aa2e10b035
Tate Keller <tatek1993@gmail.com>
2020-10-02 18:27:54 -0500

    all elements in landing

--- src/components/landing.js
+++ src/components/landing.js
@@ -5,18 +5,27 @@ const Landing = () => {
 
 
     return (
-        <div className="landing">
-            <div className='desktop-landing-info'>
-                <h1>More than just<br></br> shorter links</h1>
-                <p className='desktop-landing-txt'>
-                    Build your brand’s recognition and get detailed <br></br>
-                    insights on how your links are performing.
-                </p>
-                <p className='button'>Get Started</p>
+        <div>
+            <div className="landing">
+                <div className='desktop-landing-info'>
+                    <h1>More than just<br></br> shorter links</h1>
+                    <p className='desktop-landing-txt'>
+                        Build your brand’s recognition and get detailed <br></br>
+                        insights on how your links are performing.
+                    </p>
+                    <p className='button'>Get Started</p>
+                </div>
+                <div>
+                    <img src={Working} alt="More than just shorter links" />
+                </div>
             </div>
-            <div>
-                <img src={Working} alt="More than just shorter links" />
+            <div className='link-bar-container'>
+                <div className='link-bar'>
+                    <input placeholder='Shorten a link here...'></input>
+                    <p className='input-button'>Shorten It!</p>
+                </div>
             </div>
+
         </div>
     )
 }
