843d3e185ad1a451a032776ec1291c98be50f184
Tate Keller <tatek1993@gmail.com>
2020-10-05 20:15:07 -0500

    aligning boxes in statistics

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -12,15 +12,15 @@ const Statistics = () => {
                 </p>
             </div>
             <div className='desktop-stats-bottom'>
-                <div className='box-1'>
+                <div className='box-1 stat-box'>
                     <h3>Brand Recognition</h3>
                     <p>Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.</p>
                 </div>
-                <div className='box-2'>
+                <div className='box-2 stat-box'>
                     <h3>Detailed Records</h3>
                     <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
                 </div>
-                <div className='box-3'>
+                <div className='box-3 stat-box'>
                     <h3>Fully Customizable</h3>
                     <p>Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</p>
                 </div>
